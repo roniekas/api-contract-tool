5d0ff487ca0df20d890d22b1afa679b03e08c209
Roni Eka S <roni_eka.konsultan@work.bri.co.id>
2024-11-21 16:21:56 +0700

    adding login and redirect home

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,23 +1,36 @@
 "use client";
 
-import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import {useSession} from "next-auth/react";
+import {useRouter} from "next/navigation";
+import {useEffect} from "react";
 
 export default function Dashboard() {
-    const { data: session, status } = useSession();
+    const {data: session, status} = useSession();
     const router = useRouter();
 
     useEffect(() => {
-        if (status === "authenticated" && !session || !session) {
+        console.log({status, session});
+        if (status === "authenticated" && !session || !session && status !== "loading") {
+            console.log('wol');
             router.replace("/auth/signin");
         }
-    }, [session, status, router]); // Dependencies ensure this effect runs correctly
+    }, [session, status, router]);
+
+    function logout() {
+        router.replace("/auth/signout");
+    }
+
+    function home() {
+        router.replace("/");
+    }
 
     if (status === "loading") return <p>Loading..</p>;
 
-    // Avoid rendering content until the redirection logic has been processed
     if (status === "unauthenticated") return null;
 
-    return <div>Welcome to your dashboard, {session?.user?.name}</div>;
+    return <div>Welcome to your dashboard, {session?.user?.name} {" "}
+        <button onClick={() => logout()}>Logout</button>
+        {" "}
+        <button onClick={() => home()}>Home</button>
+    </div>;
 }
